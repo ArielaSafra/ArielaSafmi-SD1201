b75d2f4c0f4a19a7695af17744b4dec984718b45
riell <154486090+ArielaSafra@users.noreply.github.com>
2024-05-17 10:18:53 +0700

    Add files via upload

---
+++ ArielaSafmiOOP21.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+
+    // Deklarasi kelas
+    class Penjualan {
+public:
+    string nama; 
+    int banyak;
+    string jenis;
+    }
+
+    // Metode untuk menampilkan Penjualan_Kue
+    void tampilkanInfo() {
+        cout << "nama: " << nama << end1;
+        cout << "banyak: " << nama << end1;
+        cout << "jenis: " << nama << end1;
+};
+
+int main() {
+    
+    // Membuat objek Penjualan_kue
+    Kue k1("Croisant", 5, "pastry"); 
+    k1.kue();
+
+    // Memanggil metode untuk menampilkan informasi
+    nama.banyak.jenis();
+
+    return 0;
+}
